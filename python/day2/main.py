5c6798d037c79aad991f88243a155eb93a96604a
Stefan <stefan.reichel@erweka.com>
2025-04-30 14:18:53 +0200

    d2 part two done

--- python/day2/main.py
+++ python/day2/main.py
@@ -3,21 +3,25 @@ from typing import Iterable
 from utils import get_lines_as_list
 
 
-def part_one_it(lists: Iterable[list[int]]) -> int:
-    num_of_safe_reports = 0
-    for numbers in lists:
-        if is_report_safe(numbers):
-            num_of_safe_reports += 1
-    return num_of_safe_reports
+def part_two(lists: Iterable[list[int]]) -> int:
+    return sum(map(lambda numbers: is_report_safe(numbers, compare_adjacent_numbers_allow_one_wrong), lists))
+
+
+def is_report_safe(numbers: list[int], compare_adjacent_numbers_func) -> bool:
+    return (compare_adjacent_numbers_func(numbers, lambda x, y: y - 3 <= x <= y - 1) or
+            compare_adjacent_numbers_func(numbers, lambda x, y: y + 1 <= x <= y + 3))
+
+
+def compare_adjacent_numbers_allow_one_wrong(numbers, comparator):
+    numbers_excluding_index = lambda i: numbers[:i] + numbers[i + 1:]
+    return any(
+        all(map(comparator, numbers_excluding_index(i), numbers_excluding_index(i)[1:]))
+        for i in range(len(numbers))
+    )
 
 
 def part_one(lists: Iterable[list[int]]) -> int:
-    return sum(map(is_report_safe, lists))
-
-
-def is_report_safe(numbers: list[int]) -> bool:
-    return ( compare_adjacent_numbers(numbers, lambda x, y: y-3 <= x <= y-1) or
-             compare_adjacent_numbers(numbers, lambda x, y: y+1 <= x <= y+3))
+    return sum(map(lambda numbers: is_report_safe(numbers, compare_adjacent_numbers), lists))
 
 
 def compare_adjacent_numbers(numbers, comparator):
@@ -25,7 +29,7 @@ def compare_adjacent_numbers(numbers, comparator):
 
 
 if __name__ == '__main__':
-    lines_as_list = get_lines_as_list()
-    # print(list(lines_as_list))
-    part_one_result = part_one(lines_as_list)
+    part_one_result = part_one(get_lines_as_list())
     print(f"Part one result:\n{part_one_result}")
+    part_two_result = part_two(get_lines_as_list())
+    print(f"Part two result:\n{part_two_result}")
