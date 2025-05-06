b8e2170c0180807d7c8465f0d7168aef1e7dfc64
stef9998 <stef9998@mail.de>
2025-05-06 19:09:14 +0200

    d6 p1 more functional solution move procedural solution to part1_proc

--- python/day6/part1.py
+++ python/day6/part1.py
@@ -1,27 +1,50 @@
-from day6.util import init_maze_findings, Coordinate
+from day6.util import init_maze_findings, Coordinate, Direction
+from file_handling import get_file_as_lines
 
 
 def main(lines: list[str]) -> int:
 
-    maze_findings = init_maze_findings(lines)
+    maze_information = init_maze_findings(lines)
 
-    maze_width = maze_findings["maze_width"]
-    maze_height = maze_findings["maze_height"]
+    maze_width = maze_information["maze_width"]
+    maze_height = maze_information["maze_height"]
 
-    obstacle_positions = maze_findings["obstacle_positions"]
-    guard_position = maze_findings["guard_start"]
-    guard_direction = maze_findings["guard_direction"]
+    obstacle_positions = maze_information["obstacle_positions"]
+    guard_position = maze_information["guard_start"]
+    guard_direction = maze_information["guard_direction"]
+
+    outside_bounds = lambda position: (
+            position[0] < 0 or position[0] >= maze_width
+            or position[1] < 0 or position[1] >= maze_height
+    )
+    get_next_guard_state = lambda position, direction: (
+        get_next_guard_state_with_obstacles(position, direction, obstacle_positions)
+    )
 
     visited: set[Coordinate] = set()
 
+
     while True:
         visited.add(guard_position)
-        next_position = (guard_position[0] + guard_direction.value[0], guard_position[1] + guard_direction.value[1])
-        while next_position in obstacle_positions:
-            guard_direction = guard_direction.turn_right()
-            next_position = (guard_position[0] + guard_direction.value[0], guard_position[1] + guard_direction.value[1])
-        if next_position[0] < 0 or next_position[0] >= maze_width or next_position[1] < 0 or next_position[1] >= maze_height:
+        guard_position, guard_direction = get_next_guard_state(guard_position, guard_direction)
+        if outside_bounds(guard_position):
             break
-        guard_position = next_position
 
     return len(visited)
+
+
+def get_next_guard_state_with_obstacles(guard_position, guard_direction, obstacle_positions):
+    next_position = calc_next_position(guard_direction, guard_position)
+    if next_position not in obstacle_positions:
+        return next_position, guard_direction
+    return guard_position, guard_direction.turn_right()
+
+
+def calc_next_position(guard_direction: Direction, guard_position: Coordinate) -> Coordinate:
+    return guard_position[0] + guard_direction.value[0], guard_position[1] + guard_direction.value[1]
+
+
+if __name__ == '__main__':
+    part_one_result = main(get_file_as_lines())
+    print(f"Part one result:\n{part_one_result}")
+    assert part_one_result == 4711, f'Expected 4711, but got {part_one_result}'
