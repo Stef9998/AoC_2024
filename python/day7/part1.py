0b3436c07d95b66d932a5cad2126dc9df82c92f7
Stefan <stefan.reichel@erweka.com>
2025-05-07 11:36:29 +0200

    d7 init

---
+++ python/day7/part1.py
@@ -0,0 +1,12 @@
+from python.file_handling import get_file_as_lines
+
+
+def main(lines: list[str]) -> int:
+
+
+
+    return 0
+
+if __name__ == '__main__':
+    part_one_result = main(get_file_as_lines())
+    print(f"Part one result:\n{part_one_result}")
