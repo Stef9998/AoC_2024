0f7a44a84cb54654ebc12aa1ddf3d2b6acbf308b
stef9998 <stef9998@mail.de>
2025-05-12 19:05:21 +0200

    added functionality to file handling class

--- python/file_handling.py
+++ python/file_handling.py
@@ -1,5 +1,8 @@
-
 def get_file_as_lines() -> list[str]:
-    with open('input.txt', 'r') as file:
+    return get_specific_file_as_lines('input.txt')
+
+
+def get_specific_file_as_lines(text_file):
+    with open(text_file, 'r') as file:
         data = file.read().splitlines()
     return data
