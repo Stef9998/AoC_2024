5bb7b530ddf97c80c70d979d6aad982f16ecf025
Stefan <stefan.reichel@erweka.com>
2025-05-15 09:28:05 +0200

    d12 init

---
+++ python/day12/part1.py
@@ -0,0 +1,13 @@
+from file_handling import get_file_as_lines, get_specific_file_as_lines
+
+
+def main(lines: list[str]) -> int:
+
+
+    return 0
+
+if __name__ == '__main__':
+    part_one_result = main(get_file_as_lines())
+    # part_one_result = main(get_specific_file_as_lines('sample_input.txt'))
+    print(f"Part one result:\n{part_one_result}")
+    # assert part_one_result ==
