8398f2fa765edb569f561228e6d26f88305b0538
Stefan <stefan.reichel@erweka.com>
2025-05-21 13:09:28 +0200

    p1 init (data_parsed)

---
+++ python/day13/part2.py
@@ -0,0 +1,13 @@
+from python.file_handling import get_file_as_lines, get_specific_file_as_lines
+
+
+def main(lines: list[str]) -> int:
+
+    return 0
+
+
+if __name__ == '__main__':
+    result = main(get_file_as_lines())
+    # result = main(get_specific_file_as_lines('sample_input.txt'))
+    print(f"Part two result:\n{result}")
+    # assert result ==
