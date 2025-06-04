ca531c325707cfaad8835398a8a818b0adb858a5
Stefan <stef9998@mail.de>
2025-06-04 10:48:28 +0200

    day 19 done (#19)
    * init & p1 recursion idea
    
    * p1 & p2 done

---
+++ python/day19/part1.py
@@ -0,0 +1,40 @@
+from day19.utils import parse_lines
+from file_handling import get_file_as_lines, get_specific_file_as_lines
+
+
+def main(lines: list[str]) -> int:
+
+    towel_options, towel_patterns = parse_lines(lines)
+
+    return sum(recursion_solution(towel_options, towel_pattern) for towel_pattern in towel_patterns)
+
+
+def recursion_solution(towel_options, towel_pattern) -> bool:
+    pattern_length = len(towel_pattern)
+
+    def recursion_step(pattern_index: int, towel_option):
+        option_length = towel_option[0]
+        if option_length > pattern_length - pattern_index:
+            return False
+        if towel_pattern[pattern_index:pattern_index + option_length] != towel_option[1]:
+            return False
+        new_index = pattern_index + option_length
+        if new_index == pattern_length:
+            return True
+        return any(recursion_step(new_index, towel_option) for towel_option in towel_options[towel_pattern[new_index]])
+
+    return any(recursion_step(0, towel_option) for towel_option in (towel_options[towel_pattern[0]]))
+
+if __name__ == '__main__':
+
+    import time
+
+    result = main(get_specific_file_as_lines('sample_input.txt'))
+    print(f"Sample one result:\n{result}")
+    assert result == 6
+
+    start_time = time.time()
+    result = main(get_file_as_lines())
+    print(f"Part one result:\n{result}")
+    assert result == 213
+    print(f"Time taken: {time.time() - start_time:.2f} seconds")
