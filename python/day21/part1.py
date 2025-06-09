7353b2ba4c7ecd2cd93596349116700e0c59a9ad
stef9998 <stef9998@mail.de>
2025-06-09 18:20:25 +0200

    p1 & p2 done

--- python/day21/part1.py
+++ python/day21/part1.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
-import day21.utils as utils
-from day21.keypad import get_all_shortest_path, PadType
+from day21.keypad import PadType
+from day21.utils import get_number_from_numpad, get_possible_keys, movements_from_keys
 from file_handling import get_specific_file_as_lines, get_file_as_lines
 
 
@@ -11,14 +11,10 @@ def main(lines: list[str]) -> int:
 
 
 def calc_result_one_code(numpad_keys: str) -> int:
-    number = utils.get_number_from_numpad(numpad_keys)
+    number = get_number_from_numpad(numpad_keys)
     pressed_keys = get_d3_keys(numpad_keys)
     keypress_length = len(pressed_keys)
-    calculation = number * keypress_length
-    # print(f"Keys: {numpad_keys}\tKeypresses: {pressed_keys}")
-    # print(f"Number: {number}\tLength: {keypress_length}\tCalculation: {calculation}")
-    # print()
-    return calculation
+    return number * keypress_length
 
 
 def get_d3_keys(keys: str) -> str:
@@ -27,37 +23,24 @@ def get_d3_keys(keys: str) -> str:
 
 
 def d1_calc(move: tuple[str, str]):
-    possible_paths = get_all_shortest_path(PadType.NumPad, move[0], move[1])
-    possible_keys_to_press = [''.join((step.value for step in possible_path)) + 'A' for possible_path in possible_paths]
-    possible_movements = []
-    for possible_key_to_press in possible_keys_to_press:
-        movements = movements_from_keys(possible_key_to_press)
-        foo = ''.join(d2_calc(move) for move in movements)
-        possible_movements.append(foo)
+    possible_keys_to_press = get_possible_keys(move, PadType.NumPad)
+    possible_movements = [''.join(d2_calc(move) for move in (movements_from_keys(possible_key_to_press)))
+                          for possible_key_to_press in possible_keys_to_press]
     return min(possible_movements, key=len)
 
 
 @lru_cache()
 def d2_calc(move: tuple[str, str]):
-    possible_paths = get_all_shortest_path(PadType.DirectionPad, move[0], move[1])
-    possible_keys_to_press = [''.join((step.value for step in possible_path)) + 'A' for possible_path in possible_paths]
-    possible_movements = []
-    for possible_key_to_press in possible_keys_to_press:
-        movements = movements_from_keys(possible_key_to_press)
-        foo = ''.join(d3_calc(move) for move in movements)
-        possible_movements.append(foo)
+    possible_keys_to_press = get_possible_keys(move, PadType.DirectionPad)
+    possible_movements = [''.join(d3_calc(move) for move in (movements_from_keys(possible_key_to_press)))
+                          for possible_key_to_press in possible_keys_to_press]
     return min(possible_movements, key=len)
 
 
 @lru_cache()
 def d3_calc(move: tuple[str, str]):
-    possible_paths = get_all_shortest_path(PadType.DirectionPad, move[0], move[1])
-    possible_keys_to_press = [''.join((step.value for step in possible_path)) + 'A' for possible_path in possible_paths]
+    possible_keys_to_press = get_possible_keys(move, PadType.DirectionPad)
     return possible_keys_to_press[0]
-
-
-def movements_from_keys(d1_keys: str):
-    return list(zip('A' + d1_keys, d1_keys))
 
 
 if __name__ == '__main__':
@@ -70,6 +53,5 @@ if __name__ == '__main__':
     start_time = time.time()
     result = main(get_file_as_lines())
     print(f"Part one result:\n{result}")
-    assert result > 103026
-    assert result < 126384
+    assert result == 105458
     print(f"Time taken: {time.time() - start_time:.2f} seconds")
--- python/day21/utils.py
+++ python/day21/utils.py
@@ -1,4 +1,5 @@
 import list_handling
+from day21.keypad import get_all_shortest_path
 
 
 def parse_lines(lines: list[str]):
@@ -30,3 +31,13 @@ def get_number_from_numpad(keys: str):
     number = keys[:-1]
     assert number.isdigit(), f"Not a valid integer: {number}"
     return int(number)
+
+
+def get_possible_keys(move, pad_type):
+    possible_paths = get_all_shortest_path(pad_type, move[0], move[1])
+    possible_keys_to_press = [''.join((step.value for step in possible_path)) + 'A' for possible_path in possible_paths]
+    return possible_keys_to_press
+
+
+def movements_from_keys(d1_keys: str):
+    return list(zip('A' + d1_keys, d1_keys))
