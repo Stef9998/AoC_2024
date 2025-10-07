694f43aec38f7ca32ee20d095bd8441546555851
Stefan <stefan.reichel@erweka.com>
2025-10-07 10:53:42 +0200

    part2 done

--- go/aoc2024/day4/main.go
+++ go/aoc2024/day4/main.go
@@ -20,8 +20,8 @@ func main() {
 
 	res1 := part1(data)
 	fmt.Println("Part 1:", res1)
-	//res2 := part2(data)
-	//fmt.Println("Part 2:", res2)
+	res2 := part2(data)
+	fmt.Println("Part 2:", res2)
 }
 
 func part1(input [][]rune) int {
@@ -96,5 +96,58 @@ func stringFound(input string) bool {
 }
 
 func part2(input [][]rune) int {
-	return 0
+	count := 0
+	for i := 0; i < len(input)-2; i++ {
+		split := input[i : i+3]
+		found, err := linesxxmas(split)
+		if err != nil {
+			panic(err)
+		}
+		count += found
+	}
+	return count
+}
+
+func linesxxmas(input [][]rune) (int, error) {
+	found := 0
+	if len(input) != 3 {
+		return 0, errors.New("input must have exactly 3 lines")
+	}
+	for i := 0; i < len(input[0])-2; i++ {
+		split := [][]rune{input[0][i : i+3], input[1][i : i+3], input[2][i : i+3]}
+		isxx, err := isXXmas(split)
+		if err != nil {
+			return 0, err
+		}
+		if isxx {
+			found++
+		}
+	}
+	return found, nil
+}
+
+func isXXmas(input [][]rune) (bool, error) {
+	if len(input) != 3 || len(input[0]) != 3 {
+		return false, errors.New("input must be 3x3 char")
+	}
+	if input[1][1] != 'A' {
+		return false, nil
+	}
+	nw := input[0][0]
+	no := input[0][2]
+	sw := input[2][0]
+	so := input[2][2]
+	if nw == so || no == sw {
+		return false, nil
+	}
+	notSM := func(char rune) bool {
+		if char == 'S' || char == 'M' {
+			return false
+		}
+		return true
+	}
+	if notSM(nw) || notSM(no) || notSM(sw) || notSM(so) {
+		return false, nil
+	}
+	return true, nil
 }
